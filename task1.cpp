40232d7f5db86072124b4b69fdba1aedf92f9263
AmanBhatt2401 <79921956+AmanBhatt2401@users.noreply.github.com>
2021-09-30 10:41:23 +0530

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+void charFunc()
+{
+  char c='A';
+  cout<<c<<endl;
+ }
+  void boolFunc()
+ {
+   bool b=-1;
+   if(b==true)
+   {
+    b==false;
+    }
+   cout<<b<<endl;
+   }
+  void shortFunc()
+  {
+     short s=2;
+     cout<<s<<endl;
+     }
+    void intFunc()
+    {
+     int i=10;
+     cout<<i<<endl;
+     }
+     void longFunc()
+     {
+     long l=11111;
+     cout<<l<<endl;
+     }
+    void floatFunc()
+    {
+    float f=1.11f;
+    cout<<f<<endl;
+    }
+    void doubleFunc()
+    {
+    double d=10.1111;
+    cout<<d<<endl;
+    }
+    void longDoubleFunc()
+    {
+    long double ld=1.111111111111111 ;
+    cout<<ld<<endl;
+    }
+    void wideFunc()
+    {
+    wchar_t w=L'A';
+    cout<<w<<endl;
+    }
+      int main()
+   {
+      charFunc();
+      boolFunc();
+      shortFunc();
+      intFunc();
+      longFunc();
+      floatFunc();
+      doubleFunc();
+      longDoubleFunc();
+      wideFunc();
+      return 0;
+     }
+        
+ 
